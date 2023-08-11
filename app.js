4e7fac719dd2c2c1e8bcf08891db2393b683da38
Emanuel Leite <emanuel.leite10@outlook.com>
2023-08-11 12:03:30 -0600

    Adding Map Render
    Still working on bug that circle, pointer and popup; overlaps when you request a new location

--- app.js
+++ app.js
@@ -1,18 +1,87 @@
 // GET - API GeoIpify
-const callGeoIpify = () => {
-    return fetch('https://geo.ipify.org/api/v2/country,city?apiKey=at_GGvVwQpnkx7ZlyALmgX1MDy8NsACf&ipAddress=190.143.246.119')
+const callGeoIpify = (ipAddress) => {
+    return fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_GGvVwQpnkx7ZlyALmgX1MDy8NsACf&ipAddress=${ipAddress}`)
         .then(response => response.json())
         .then(data => {
-            let lat = data.location.lat;
-            let lng = data.location.lng;
-            return [lat, lng]
+            return data.location
         })
         .catch(error => {
             console.error('Error:', error);
         });
 }
 
-const func = async () => {
-    const result = await callGeoIpify()
-    console.log(result)
+// Generating the map
+const renderMap = async (ipAddress) => {
+
+    let data = await callGeoIpify(ipAddress)
+    let lat = data.lat
+    let lng = data.lng
+    let region = data.region
+    let city = data.city
+    let country = data.country
+
+    map.setView([lat, lng], 15)
+
+    // Elimina objetos anteriores si existen
+    if (marker){
+        marker.remove()
+    }
+    if (circle){
+        circle.remove()
+    }
+
+    if (popup){
+        popup.remove()
+    }
+
+
+    // Location
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '© OpenStreetMap'
+    }).addTo(map);
+
+    // Marker
+    var marker = L.marker([lat, lng]).addTo(map);
+
+    // Circle
+    var circle = L.circle([lat, lng], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 500
+    }).addTo(map);
+
+    // Popups
+    marker.bindPopup(`<b>${city}</b><br>${country}`).openPopup();
+
+    // Popup Click event
+    var popup = L.popup();
+    function onMapClick(e) {
+        popup
+            .setLatLng(e.latlng)
+            .setContent("You clicked the map at " + e.latlng.toString())
+            .openOn(map);
+    }
+
+    map.on('click', onMapClick);
 }
+
+// Event Listener
+const button = document.querySelector("#submit")
+const inputText = document.querySelector("#input")
+
+
+button.addEventListener("click", (e) => {
+    e.preventDefault() // Evita que recargue la ventana
+    let ipAddress = inputText.value
+    renderMap(ipAddress)
+})
+
+// Initialing the map
+var map = L.map('map').setView([0, 0], 2.5);
+// Location
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
